4c907ccf7bae238999bbbc70d3c6c9f0d1ed26a7
Anthony Castelli <neueanthony@me.com>
2017-05-16 22:54:33 -0700

    tokens: Token creation for customers

--- Sources/API/Routes/TokenRoutes.swift
+++ Sources/API/Routes/TokenRoutes.swift
@@ -81,6 +81,30 @@ public final class TokenRoutes {
     }
     
     /**
+     Create a payment token from a customer
+     
+     - parameter customer: The customer to generate the token with
+     
+     - returns: A StripeRequest<> item which you can then use to convert to the corresponding node
+    */
+    public func createToken(withCustomer customer: Customer) throws -> StripeRequest<Token> {
+        let body = try Node(node: ["customer": customer.id])
+        return try StripeRequest(client: self.client, method: .post, route: .tokens, body: Body.data(body.formURLEncoded()))
+    }
+    
+    /**
+     Create a payment token from a customer
+     
+     - parameter customerId: The customer ID to generate the token with
+     
+     - returns: A StripeRequest<> item which you can then use to convert to the corresponding node
+     */
+    public func createToken(withCustomer customerId: String) throws -> StripeRequest<Token> {
+        let body = try Node(node: ["customer": customerId])
+        return try StripeRequest(client: self.client, method: .post, route: .tokens, body: Body.data(body.formURLEncoded()))
+    }
+    
+    /**
      Retrieve a token
      Retrieves the token with the given ID.
      
