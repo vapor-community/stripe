88874c07a8122b336b9d7c4782ea59f10e1af16f
Andrew <andrewangeta@gmail.com>
2017-05-29 10:35:15 -0400

    Removed tokens and id's. Tests now create and use tokens and id's dynamically.

--- Tests/StripeTests/Test+Droplet.swift
+++ Tests/StripeTests/Test+Droplet.swift
@@ -11,29 +11,11 @@ import XCTest
 import Vapor
 import Stripe
 
-// Used for fetching a specific Transaction in the history
-let TestTransactionID = "txn_1A9KJiJm6xi4tXu1EkbD0ZEC"
-
-// Used for creating charges
-let TestChargeSourceTokenID = "tok_1A9M61Jm6xi4tXu1sSQMnsxp"
-
-// Used for fetching a specific charge
-let TestChargeID = "ch_1A9M5oJm6xi4tXu1iWO7wxMy"
-
-// Used for fetching a specific customer
-let TestCustomerID = "cus_AVqRAm2Gv8EMyd"
-
-// Used for token testing
-let TestTokenID = "tok_1AIiMpJm6xi4tXu1hoimk73q"
-
-// Used for refund testing
-let TestRefundID = "re_1AJ4FFJm6xi4tXu1YFFB9N7P"
-
 extension XCTestCase {
     func makeDroplet() throws -> Droplet {
         let config = Config([
             "stripe": [
-                "apiKey": "sk_test_Pb8SpX80t1XhJengpQ3oE67H" // Add your own API Key for tests
+                "apiKey": "" // Add your own API Key for tests
             ],
         ])
         try config.addProvider(Stripe.Provider.self)
