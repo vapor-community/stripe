52737ce433ff7440e005e9d79bed169ffdc055e6
Andrew <andrewangeta@gmail.com>
2017-05-29 10:26:47 -0400

    Added url property.

--- Sources/Models/Customer/CustomerList.swift
+++ Sources/Models/Customer/CustomerList.swift
@@ -13,11 +13,13 @@ import Helpers
 public final class CustomerList: StripeModelProtocol {
     
     public let object: String
+    public let url: String
     public let hasMore: Bool
     public let items: [Customer]?
     
     public init(node: Node) throws {
         self.object = try node.get("object")
+        self.url = try node.get("url")
         self.hasMore = try node.get("has_more")
         self.items = try node.get("data")
     }
@@ -25,10 +27,10 @@ public final class CustomerList: StripeModelProtocol {
     public func makeNode(in context: Context?) throws -> Node {
         let object: [String : Any?] = [
             "object": self.object,
+            "url": self.url,
             "has_more": self.hasMore,
             "data": self.items
         ]
         return try Node(node: object)
     }
-    
 }
