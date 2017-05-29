9949aeffca9c0796460c1614ce07f0664200296e
Andrew <andrewangeta@gmail.com>
2017-05-29 10:35:42 -0400

    Updated tests

--- Tests/StripeTests/TokenTests.swift
+++ Tests/StripeTests/TokenTests.swift
@@ -20,22 +20,50 @@ class TokenTests: XCTestCase {
         ("testBankAccountCreation", testBankAccountCreation),
     ]
 
-    func testTokenCreation() throws {
-        let drop = try self.makeDroplet()
-        let object = try drop.stripe?.tokens.createCard(withCardNumber: "4242 4242 4242 4242", expirationMonth: 10, expirationYear: 2018, cvc: 123, name: "Test Card").serializedResponse()
+    var drop: Droplet?
+    var tokenId: String = ""
+    
+    override func setUp()
+    {
+        do
+        {
+            drop = try self.makeDroplet()
+            
+            tokenId = try drop?.stripe?.tokens.createCard(withCardNumber: "4242 4242 4242 4242",
+                                                             expirationMonth: 10,
+                                                             expirationYear: 2018,
+                                                             cvc: 123,
+                                                             name: "Test Card").serializedResponse().id ?? ""
+        }
+        catch
+        {
+            fatalError("Setup failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func testTokenCreation() throws
+    {
+        let object = try drop?.stripe?.tokens.createCard(withCardNumber: "4242 4242 4242 4242",
+                                                         expirationMonth: 10,
+                                                         expirationYear: 2018,
+                                                         cvc: 123,
+                                                         name: "Test Card").serializedResponse()
         XCTAssertNotNil(object?.card)
     }
     
-    func testTokenRetrieval() throws {
-        let drop = try self.makeDroplet()
-        let object = try drop.stripe?.tokens.retrieve(TestTokenID).serializedResponse()
+    func testTokenRetrieval() throws
+    {
+        let object = try drop?.stripe?.tokens.retrieve(tokenId).serializedResponse()
         XCTAssertNotNil(object)
     }
     
     func testBankAccountCreation() throws {
-        let drop = try self.makeDroplet()
-        let object = try drop.stripe?.tokens.createBank(country: "US", currency: .usd, accountNumber: "000123456789", routingNumber: "110000000", accountHolderName: "Test Person", accountHolderType: "Individual").serializedResponse()
+        let object = try drop?.stripe?.tokens.createBank(country: "US",
+                                                         currency: .usd,
+                                                         accountNumber: "000123456789",
+                                                         routingNumber: "110000000",
+                                                         accountHolderName: "Test Person",
+                                                         accountHolderType: "Individual").serializedResponse()
         XCTAssertNotNil(object?.bankAccount)
     }
-    
 }
