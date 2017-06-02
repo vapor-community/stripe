f52de85d0054c490d0df01bb879225baf297612a
Andrew <andrewangeta@gmail.com>
2017-06-02 19:12:40 -0400

    Clean up/safety.

--- Sources/Models/Charges/Outcome.swift
+++ Sources/Models/Charges/Outcome.swift
@@ -10,7 +10,7 @@ import Foundation
 import Vapor
 import Helpers
 
-/*
+/**
  Outcome
  https://stripe.com/docs/api/curl#charge_object-outcome
  */
@@ -39,31 +39,37 @@ public enum OutcomeType: String {
 
 public final class Outcome: StripeModelProtocol {
     
-    public let networkStatus: NetworkStatus?
-    public let reason: String?
-    public let riskLevel: RiskLevel?
-    public let rule: String?
-    public let sellerMessage: String
-    public let type: OutcomeType?
+    public private(set) var networkStatus: NetworkStatus?
+    public private(set) var reason: String?
+    public private(set) var riskLevel: RiskLevel?
+    public private(set) var rule: String?
+    public private(set) var sellerMessage: String?
+    public private(set) var type: OutcomeType?
     
     public init(node: Node) throws {
-        self.networkStatus = try NetworkStatus(rawValue: node.get("network_status"))
+        if let networkStatus = node["network_status"]?.string {
+            self.networkStatus = NetworkStatus(rawValue: networkStatus)
+        }
         self.reason = try node.get("reason")
-        self.riskLevel = try RiskLevel(rawValue: node.get("risk_level"))
+        if let riskLevel = node["risk_level"]?.string {
+            self.riskLevel = RiskLevel(rawValue: riskLevel)
+        }
         self.rule = try node.get("rule")
         self.sellerMessage = try node.get("seller_message")
-        self.type = try OutcomeType(rawValue: node.get("type"))
+        if let outcome = node["type"]?.string {
+            self.type = OutcomeType(rawValue: outcome)
+        }
     }
     
     public func makeNode(in context: Context?) throws -> Node {
-        return try Node(node: [
+        let object: [String : Any?] = [
             "network_status": self.networkStatus?.rawValue,
             "reason": self.reason ?? nil,
             "risk_level": self.riskLevel?.rawValue,
             "rule": self.rule ?? nil,
             "seller_message": self.sellerMessage,
             "type": self.type?.rawValue
-        ])
+        ]
+        return try Node(node: object)
     }
-    
 }
