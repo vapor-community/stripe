a3946b5f195d555418cf718e4e261091a0100a86
Andrew <andrewangeta@gmail.com>
2017-06-02 18:41:56 -0400

    Mode model read only.

--- Sources/Models/Balance/BalanceHistoryList.swift
+++ Sources/Models/Balance/BalanceHistoryList.swift
@@ -11,22 +11,25 @@ import Vapor
 
 public final class BalanceHistoryList: StripeModelProtocol {
     
-    public let object: String
-    public let hasMore: Bool
-    public let items: [BalanceTransactionItem]
+    public private(set) var object: String?
+    public private(set) var url: String?
+    public private(set) var hasMore: Bool?
+    public private(set) var items: [BalanceTransactionItem]?
     
     public init(node: Node) throws {
         self.object = try node.get("object")
+        self.url = try node.get("url")
         self.hasMore = try node.get("has_more")
         self.items = try node.get("data")
     }
     
     public func makeNode(in context: Context?) throws -> Node {
-        return try Node(node: [
+        let object: [String : Any?] = [
             "object": self.object,
+            "url": self.url,
             "has_more": self.hasMore,
             "data": self.items
-        ])
+        ]
+        return try Node(node: object)
     }
-    
 }
