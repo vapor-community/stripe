9352392d1572f27b92b3682d35e549325672f94b
Anthony Castelli <neueanthony@me.com>
2017-09-06 10:54:11 -0700

    invoice: Fix up a route declaration

--- Sources/Stripe/API/Routes/InvoiceRoutes.swift
+++ Sources/Stripe/API/Routes/InvoiceRoutes.swift
@@ -262,7 +262,7 @@ public final class InvoiceRoutes {
      
      - returns: A StripeRequest<> item which you can then use to convert to the corresponding node
      */
-    public func listAll(customer: String? = nil, filter: StripeFilter?) throws -> StripeRequest<InvoiceList> {
+    public func listAll(customer: String? = nil, filter: StripeFilter? = nil) throws -> StripeRequest<InvoiceList> {
         var query = [String : NodeRepresentable]()
         if let customer = customer {
             query["customer"] = customer
