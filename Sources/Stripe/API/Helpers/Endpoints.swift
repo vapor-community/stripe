6cf37dcd7f7b312d858924305e3b1ce49d6132c0
Anthony Castelli <neueanthony@me.com>
2017-09-05 17:58:39 -0700

    invoices: Add endpoints for invoice items

--- Sources/Stripe/API/Helpers/Endpoints.swift
+++ Sources/Stripe/API/Helpers/Endpoints.swift
@@ -177,6 +177,16 @@ internal enum API {
     case invoiceLines(String)
     case upcomingInvoices
     
+    /**
+     INVOICE ITEMS
+     Sometimes you want to add a charge or credit to a customer but only actually charge the customer's card at 
+     the end of a regular billing cycle. This is useful for combining several charges to minimize per-transaction 
+     fees or having Stripe tabulate your usage-based billing totals.
+    */
+    case invoiceItems
+    case invoiceItem(String)
+    
+    
     var endpoint: String {
         switch self {
         case .balance: return APIBase + APIVersion + "balance"
@@ -242,6 +252,9 @@ internal enum API {
         case .payInvoice(let id): return APIBase + APIVersion + "invoices/\(id)/pay"
         case .invoiceLines(let id): return APIBase + APIVersion + "invoices/\(id)/lines"
         case .upcomingInvoices: return APIBase + APIVersion + "invoices/upcoming"
+            
+        case .invoiceItems:return APIBase + APIVersion + "invoiceitems"
+        case .invoiceItem(let id): return APIBase + APIVersion + "invoiceitems/\(id)"
         }
     }
 }
