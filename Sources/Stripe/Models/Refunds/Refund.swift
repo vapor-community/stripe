8426bdc0cfa92dae870a9b8d7d46e1552b8a3cf5
Andrew Edwards <andrewangeta@gmail.com>
2017-12-10 12:25:40 -0500

    Updated Refund for Vapor 3

---
+++ Sources/Stripe/Models/Refunds/Refund.swift
@@ -0,0 +1,59 @@
+//
+//  RefundItem.swift
+//  Stripe
+//
+//  Created by Anthony Castelli on 4/15/17.
+//
+//
+
+import Foundation
+
+/**
+ Refund object
+ https://stripe.com/docs/api/curl#refunds
+ */
+
+public protocol Refund {
+    var id: String? { get }
+    var object: String? { get }
+    var amount: Int? { get }
+    var balanceTransactionId: String? { get }
+    var charge: String? { get }
+    var created: Date? { get }
+    var currency: StripeCurrency? { get }
+    var description: String? { get }
+    var metadata: [String: String]? { get }
+    var reason: RefundReason? { get }
+    var receiptNumber: String? { get }
+    var status: StripeStatus? { get }
+}
+
+public struct StripeRefund: Refund, StripeModelProtocol {
+    public var id: String?
+    public var object: String?
+    public var amount: Int?
+    public var balanceTransactionId: String?
+    public var charge: String?
+    public var created: Date?
+    public var currency: StripeCurrency?
+    public var description: String?
+    public var metadata: [String: String]?
+    public var reason: RefundReason?
+    public var receiptNumber: String?
+    public var status: StripeStatus?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case object
+        case amount
+        case balanceTransactionId = "balance_transaction"
+        case charge
+        case created
+        case currency
+        case description
+        case metadata
+        case reason
+        case receiptNumber = "receipt_number"
+        case status
+    }
+}
---
+++ Sources/Stripe/Models/Refunds/RefundReason.swift
@@ -0,0 +1,24 @@
+//
+//  RefundReason.swift
+//  Stripe
+//
+//  Created by Anthony Castelli on 5/13/17.
+//
+//
+
+/**
+ Refund reason
+ https://stripe.com/docs/api/curl#refund_object-reason
+ */
+
+public enum RefundReason: String, Codable {
+    case duplicate
+    case fraudulent
+    case requestedByCustomer
+    
+    enum CdingKeys: String, CodingKey {
+        case duplicate
+        case fraudulent
+        case requestedByCustomer = "requested_by_customer"
+    }
+}
