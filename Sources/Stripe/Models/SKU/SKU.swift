409e6d8badd719d91db892a2cf22a1c393a569d3
Andrew Edwards <andrewangeta@gmail.com>
2018-01-15 12:07:49 -0500

    Updated SKU API for Vapor 3

--- Sources/Stripe/Models/SKU/SKU.swift
+++ Sources/Stripe/Models/SKU/SKU.swift
@@ -7,67 +7,62 @@
 //
 
 import Foundation
-import Vapor
 
 /**
  SKU object
  https://stripe.com/docs/api#skus
  */
 
-open class SKU: StripeModelProtocol {
+public protocol SKU {
+    associatedtype PD: PackageDimensions
+    associatedtype I: Inventory
     
-    public private(set) var id: String?
-    public private(set) var object: String?
-    public private(set) var active: Bool?
-    public private(set) var attributes: Node?
-    public private(set) var created: Date?
-    public private(set) var currency: StripeCurrency?
-    public private(set) var image: String?
-    public private(set) var inventory: Inventory?
-    public private(set) var isLive: Bool?
-    public private(set) var metadata: Node?
-    public private(set) var packageDimensions: PackageDimensions?
-    public private(set) var price: Int?
-    public private(set) var product: String?
-    public private(set) var updated: Date?
+    var id: String? { get }
+    var object: String? { get }
+    var active: Bool? { get }
+    var attributes: [String: String]? { get }
+    var created: Date? { get }
+    var currency: StripeCurrency? { get }
+    var image: String? { get }
+    var inventory: I? { get }
+    var isLive: Bool? { get }
+    var metadata: [String: String]? { get }
+    var packageDimensions: PD? { get }
+    var price: Int? { get }
+    var productId: String? { get }
+    var updated: Date? { get }
+}
+
+public struct StripeSKU: SKU, StripeModelProtocol {
+    public var id: String?
+    public var object: String?
+    public var active: Bool?
+    public var attributes: [String: String]?
+    public var created: Date?
+    public var currency: StripeCurrency?
+    public var image: String?
+    public var inventory: StripeInventory?
+    public var isLive: Bool?
+    public var metadata: [String: String]?
+    public var packageDimensions: StripePackageDimensions?
+    public var price: Int?
+    public var productId: String?
+    public var updated: Date?
     
-    public required init(node: Node) throws {
-        self.id = try node.get("id")
-        self.object = try node.get("object")
-        self.active = try node.get("active")
-        self.attributes = try node.get("attributes")
-        self.created = try node.get("created")
-        if let currency = node["currency"]?.string {
-            self.currency = StripeCurrency(rawValue: currency)
-        }
-        self.image = try node.get("image")
-        self.inventory = try node.get("inventory")
-        self.isLive = try node.get("livemode")
-        self.metadata = try node.get("metadata")
-        self.packageDimensions = try node.get("package_dimensions")
-        self.price = try node.get("price")
-        self.product = try node.get("product")
-        self.updated = try node.get("updated")
-    }
-    
-    public func makeNode(in context: Context?) throws -> Node {
-        
-        let object: [String: Any?] = [
-            "id": self.id,
-            "object": self.object,
-            "active": self.active,
-            "attributes": self.attributes,
-            "created": self.created,
-            "currency": self.currency?.rawValue,
-            "image": self.image,
-            "inventory": self.inventory,
-            "livemode": self.isLive,
-            "metadata": self.metadata,
-            "package_dimensions": self.packageDimensions,
-            "price": self.price,
-            "product": self.product,
-            "updated": self.updated
-        ]
-        return try Node(node: object)
+    enum CodngKeys: String, CodingKey {
+        case id
+        case object
+        case active
+        case attributes
+        case created
+        case currency
+        case image
+        case inventory
+        case isLive = "livemode"
+        case metadata
+        case packageDimensions = "package_dimensions"
+        case price
+        case productId = "product"
+        case updated
     }
 }
