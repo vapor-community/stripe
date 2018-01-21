0fd0dfe9c0c070db60dc244a3366f734d47714ab
Andrew Edwards <andrewangeta@gmail.com>
2018-01-21 18:26:44 -0500

    Updated route for for Vapor 3

--- Sources/Stripe/API/Routes/BalanceRoutes.swift
+++ Sources/Stripe/API/Routes/BalanceRoutes.swift
@@ -6,52 +6,40 @@
 //
 //
 
-import Node
+import Vapor
 
-open class BalanceRoutes {
+public protocol BalanceRoutes {
+    associatedtype B: Balance
+    associatedtype BT: BalanceTransactionItem
+    associatedtype BHL: List
     
-    let client: StripeClient
+    func retrieve() throws -> Future<B>
+    func retrieve(forTransaction: String) throws -> Future<BT>
+    func listAll(filter: [String: Any]) throws -> Future<BHL>
+}
+
+public struct StripeBalanceRoutes<SR>: BalanceRoutes where SR: StripeRequest {
+    private let request: SR
     
-    init(client: StripeClient) {
-        self.client = client
+    init(request: SR) {
+        self.request = request
     }
     
-    /**
-     Retrieve balance
-     Retrieves the current account balance, based on the authentication that was used to make the request.
-     
-     - returns: A StripeRequest<> item which you can then use to convert to the corresponding node
-    */
-    public func retrieveBalance() throws -> StripeRequest<Balance> {
-        return try StripeRequest(client: self.client, method: .get, route: .balance, query: [:], body: nil, headers: nil)
+    /// Retrieve balance
+    /// [Learn More →](https://stripe.com/docs/api/curl#retrieve_balance)
+    public func retrieve() throws -> Future<StripeBalance> {
+        return try request.send(method: .get, path: StripeAPIEndpoint.balance.endpoint)
     }
     
-    /**
-     Retrieve a balance transaction
-     Retrieves the balance transaction with the given ID.
-     
-     - parameter transactionId: The ID of the desired balance transaction (as found on any API object that affects the balance, e.g. a charge or transfer).
-     
-     - returns: A StripeRequest<> item which you can then use to convert to the corresponding node
-     */
-    public func retrieveBalance(forTransaction transactionId: String) throws -> StripeRequest<BalanceTransactionItem> {
-        return try StripeRequest(client: self.client, method: .get, route: .balanceHistoryTransaction(transactionId), query: [:], body: nil, headers: nil)
+    /// Retrieve a balance transaction
+    /// [Learn More →](https://stripe.com/docs/api/curl#balance_transaction_retrieve)
+    public func retrieve(forTransaction transactionId: String) throws -> Future<StripeBalanceTransactionItem> {
+        return try request.send(method: .get, path: StripeAPIEndpoint.balanceHistoryTransaction(transactionId).endpoint)
     }
     
-    /**
-     List all balance history
-     Returns a list of transactions that have contributed to the Stripe account balance (e.g., charges, transfers, and so forth). 
-     The transactions are returned in sorted order, with the most recent transactions appearing first.
-     
-     - parameter filter: A Filter item to pass query parameters when fetching results
-     
-     - returns: A StripeRequest<> item which you can then use to convert to the corresponding node
-     */
-    public func history(forFilter filter: StripeFilter? = nil) throws -> StripeRequest<BalanceHistoryList> {
-        var query = [String : NodeRepresentable]()
-        if let data = try filter?.createQuery() {
-            query = data
-        }
-        return try StripeRequest(client: self.client, method: .get, route: .balanceHistory, query: query, body: nil, headers: nil)
+    /// List all balance history
+    /// [Learn More →](https://stripe.com/docs/api/curl#balance_history)
+    public func listAll(filter: [String: Any]) throws -> Future<BalanceHistoryList> {
+        return try request.send(method: .get, path: StripeAPIEndpoint.account.endpoint, query: filter.queryParameters)
     }
 }
