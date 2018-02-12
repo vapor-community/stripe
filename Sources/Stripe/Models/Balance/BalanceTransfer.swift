258ff4dc52e5098bd03dce94eb9b92a3e5293a19
Andrew Edwards <andrewangeta@gmail.com>
2018-02-11 19:12:27 -0500

    Clean up.

--- Sources/Stripe/Models/Balance/BalanceTransfer.swift
+++ Sources/Stripe/Models/Balance/BalanceTransfer.swift
@@ -10,39 +10,15 @@
  Balance transfer is the body object of available array.
  https://stripe.com/docs/api/curl#balance_object
  */
-
+// TODO: - Use BalanceTransfer SourceTypes enum
 public protocol BalanceTransfer {
     var currency: StripeCurrency? { get }
     var amount: Int? { get }
-    var sourceTypes: [SourceType: Int] { get }
+    var sourceTypes: [String: Int]? { get }
 }
 
 public struct StripeBalanceTransfer: BalanceTransfer, StripeModel {
     public var currency: StripeCurrency?
     public var amount: Int?
-    public var sourceTypes: [SourceType: Int] = [:]
-    
-    enum CodingKeys: String, CodingKey {
-        case currency
-        case amount
-        case sourceTypes = "source_types"
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        currency = try container.decodeIfPresent(StripeCurrency.self, forKey: .currency)
-        amount = try container.decodeIfPresent(Int.self, forKey: .amount)
-    
-        let tempTypes = try container.decodeIfPresent([String: Int].self, forKey: .sourceTypes)
-        
-        if let types = tempTypes {
-            types.forEach { (key, value) in
-                let newKey = SourceType(rawValue: key)
-                // TODO: Figure out a way around this force unwrap.
-                sourceTypes[newKey!] = value
-            }
-        }
-
-    }
+    public var sourceTypes: [String: Int]?
 }
