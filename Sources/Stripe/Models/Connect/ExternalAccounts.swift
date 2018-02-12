df5e248a250c7e56b52f51a661db5d5eb6314308
Andrew Edwards <andrewangeta@gmail.com>
2018-02-11 19:30:28 -0500

    Finished implementation.

--- Sources/Stripe/Models/Connect/ExternalAccounts.swift
+++ Sources/Stripe/Models/Connect/ExternalAccounts.swift
@@ -16,10 +16,19 @@ public struct ExternalAccountsList: StripeModel {
     public var hasMore: Bool?
     public var totalCount: Int?
     public var url: String?
-    public var data: [[String: String]]?
-    // TODO: Implements parsing of items into typed arrays.
-    public var cardAccounts: [StripeCard] = []
-    public var bankAccounts: [StripeBankAccount] = []
+    private var data: String?
+    public var cardAccounts: [StripeCard]?
+    public var bankAccounts: [StripeBankAccount]?
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        object = try container.decodeIfPresent(String.self, forKey: .object)
+        hasMore = try container.decodeIfPresent(Bool.self, forKey: .hasMore)
+        totalCount = try container.decodeIfPresent(Int.self, forKey: .totalCount)
+        url = try container.decodeIfPresent(String.self, forKey: .url)
+        cardAccounts = try container.decodeIfPresent([StripeCard].self, forKey: .data)?.filter{ $0.object == "card" }
+        bankAccounts = try container.decodeIfPresent([StripeBankAccount].self, forKey: .data)?.filter{ $0.object == "bank_account" }
+    }
 }
 
 public protocol ExternalAccount {}
