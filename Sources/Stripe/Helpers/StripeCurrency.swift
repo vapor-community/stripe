5f9555b60e5a91377717b746d83ed2b774eb4fc2
Andrew Edwards <andrewangeta@gmail.com>
2018-02-11 19:10:55 -0500

    Added more currencies.

--- Sources/Stripe/Helpers/StripeCurrency.swift
+++ Sources/Stripe/Helpers/StripeCurrency.swift
@@ -7,14 +7,49 @@
 //
 
 public enum StripeCurrency: String, Codable {
+    case usd
+    case aed
+    case afn
+    case all
+    case amd
+    case ang
+    case aoa
+    case ars
     case aud
+    case awg
+    case azn
+    case bam
+    case bbd
+    case bdt
+    case bgn
+    case bif
+    case bmd
+    case bnd
+    case bob
+    case brl
+    case bsd
+    case bwp
+    case bzd
     case cad
+    case cdf
+    case chf
+    case clp
+    case cny
+    case cop
+    case crc
+    case cve
+    case czk
+    case djf
     case dkk
+    case dop
+    case dzd
+    // TODO: - finish adding stripe currecies
+    case egp
     case eur
     case gbp
     case jpy
     case nok
-    case usd
     case sek
+    case nzd
     case sgd
 }
