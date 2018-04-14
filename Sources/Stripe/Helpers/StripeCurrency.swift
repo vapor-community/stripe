283e6b155b57594cb9f6025c656f6aabf3aad3db
tarik <tarikstafford@gmail.com>
2018-04-14 11:07:10 +0530

    Updated StripeCurrency

--- Sources/Stripe/Helpers/StripeCurrency.swift
+++ Sources/Stripe/Helpers/StripeCurrency.swift
@@ -43,13 +43,69 @@ public enum StripeCurrency: String, Codable {
     case dkk
     case dop
     case dzd
-    // TODO: - finish adding stripe currecies
     case egp
     case eur
+    case fjd
     case gbp
+    case gel
+    case gip
+    case gmd
+    case gyd
+    case hkd
+    case hrk
+    case htg
+    case idr
     case jpy
     case nok
     case sek
     case nzd
     case sgd
+    case lkr
+    case lrd
+    case lsl
+    case mad
+    case mdl
+    case mkd
+    case mmk
+    case mnt
+    case mop
+    case mro
+    case mvr
+    case mwk
+    case mxn
+    case myr
+    case mzn
+    case nad
+    case ngn
+    case npr
+    case pgk
+    case php
+    case pkr
+    case pln
+    case qar
+    case ron
+    case rsd
+    case rub
+    case sar
+    case sbd
+    case scr
+    case sll
+    case sos
+    case std
+    case szl
+    case thb
+    case tjs
+    case top
+    case ttd
+    case twd
+    case tzs
+    case uah
+    case ugx
+    case uzs
+    case wts
+    case xcd
+    case yer
+    case zar
+    case zmw
+    // TODO: - finish adding stripe currecies
 }
