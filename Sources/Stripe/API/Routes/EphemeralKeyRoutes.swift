76ce7c75c262c50c365b421b975a8d1fdfad192d
Andrew Edwards <andrewangeta@gmail.com>
2018-06-17 14:14:57 -0400

    Update EphemeralKeyRoutes.swift

--- Sources/Stripe/API/Routes/EphemeralKeyRoutes.swift
+++ Sources/Stripe/API/Routes/EphemeralKeyRoutes.swift
@@ -9,7 +9,7 @@
 import Vapor
 
 public protocol EphemeralKeyRoutes {
-    func create(customer: String) throws -> Future<StripeEphemeralKey>
+    func create(customer: String, apiVersion: String?) throws -> Future<StripeEphemeralKey>
     func delete(ephemeralKey: String) throws -> Future<StripeEphemeralKey>
 }
 
@@ -21,14 +21,14 @@ public struct StripeEphemeralKeyRoutes: EphemeralKeyRoutes {
     }
     
     public func create(customer: String, apiVersion: String? = nil) throws -> Future<StripeEphemeralKey> {
-		var headers: HTTPHeaders = [:]
-		
-		if let otherApiVersion = apiVersion {
-			headers.replaceOrAdd(name: .stripeVersion, value: otherApiVersion)
-		}
-		
-		let body = ["customer": customer]
-		return try request.send(method: .POST, path: StripeAPIEndpoint.ephemeralKeys.endpoint, body: body.queryParameters, headers: headers)
+        var headers: HTTPHeaders = [:]
+        
+        if let otherApiVersion = apiVersion {
+            headers.replaceOrAdd(name: .stripeVersion, value: otherApiVersion)
+        }
+        
+        let body = ["customer": customer]
+        return try request.send(method: .POST, path: StripeAPIEndpoint.ephemeralKeys.endpoint, body: body.queryParameters, headers: headers)
     }
     
     public func delete(ephemeralKey: String) throws -> Future<StripeEphemeralKey> {
