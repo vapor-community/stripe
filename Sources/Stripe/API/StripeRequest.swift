1da41324fa68755d89f7e2c27972b6a496e26c41
Andrew Edwards <andrewangeta@gmail.com>
2018-08-20 13:55:47 -0400

    Updated to latest API.

--- Sources/Stripe/API/StripeRequest.swift
+++ Sources/Stripe/API/StripeRequest.swift
@@ -46,7 +46,7 @@ extension HTTPHeaderName {
 extension HTTPHeaders {
     public static var stripeDefault: HTTPHeaders {
         var headers: HTTPHeaders = [:]
-        headers.replaceOrAdd(name: .stripeVersion, value: "2018-05-21")
+        headers.replaceOrAdd(name: .stripeVersion, value: "2018-07-27")
         headers.replaceOrAdd(name: .contentType, value: MediaType.urlEncodedForm.description)
         return headers
     }
