55bd0f69633e195807d5e83a9e32e4ed92f18305
Andrew Edwards <andrewangeta@gmail.com>
2019-02-24 14:18:16 -0500

    Updated customer routes and model to latest API.

--- Sources/Stripe/Models/Customer/Customer.swift
+++ Sources/Stripe/Models/Customer/Customer.swift
@@ -17,7 +17,6 @@ public struct StripeCustomer: StripeModel {
     public var id: String
     public var object: String
     public var accountBalance: Int?
-    public var businessVatId: String?
     public var created: Date?
     public var currency: StripeCurrency?
     public var defaultSource: String?
@@ -25,17 +24,20 @@ public struct StripeCustomer: StripeModel {
     public var description: String?
     public var discount: StripeDiscount?
     public var email: String?
+    public var invoicePrefix: String?
+    public var invoiceSettings: StripeInvoiceSettings?
     public var livemode: Bool?
     public var metadata: [String: String]
     public var shipping: ShippingLabel?
     public var sources: StripeSourcesList?
     public var subscriptions: StripeSubscriptionsList?
+    public var taxInfo: StripeCustomerTaxInfo?
+    public var taxInfoVerification: StripeCustomerTaxInfo?
     
     public enum CodingKeys: String, CodingKey {
         case id
         case object
         case accountBalance = "account_balance"
-        case businessVatId = "business_vat_id"
         case created
         case currency
         case defaultSource = "default_source"
@@ -43,10 +45,49 @@ public struct StripeCustomer: StripeModel {
         case description
         case discount
         case email
+        case invoicePrefix = "invoice_prefix"
+        case invoiceSettings = "invoice_settings"
         case livemode
         case metadata
         case shipping
         case sources
         case subscriptions
+        case taxInfo = "tax_info"
+        case taxInfoVerification = "tax_info_verification"
     }
+}
+
+public struct StripeCustomerTaxInfo: StripeModel {
+    public var taxId: String?
+    public var type: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case taxId = "tax_id"
+        case type
+    }
+}
+
+public struct StripeCustomerTaxInfoVerification: StripeModel {
+    public var status: String?
+    public var verifiedName: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case status
+        case verifiedName = "verified_name"
+    }
+}
+
+public struct StripeInvoiceSettings: StripeModel {
+    public var footer: String?
+    public var customFields: [StripeInvoiceCustomFields]?
+    
+    private enum CodingKeys: String, CodingKey {
+        case footer
+        case customFields = "custom_fields"
+    }
+}
+
+public struct StripeInvoiceCustomFields: StripeModel {
+    public var name: String?
+    public var value: String?
 }
