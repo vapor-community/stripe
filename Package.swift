0cf3526bf27e2fa2c245c8c072fa75e28ab0e45e
Andrew Edwards <andrewangeta@gmail.com>
2019-06-18 23:08:07 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha"),
-        .package(url: "https://github.com/vapor-community/StripeKit.git", .branch("master")),
+        .package(url: "https://github.com/vapor-community/StripeKit.git", from: "1.0.5"),
     ],
     targets: [
         .target(name: "Stripe", dependencies: ["Vapor","StripeKit"]),
