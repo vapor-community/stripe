7ec97072d50854924650d54b59645bebac65a66e
Andrew Edwards <andrewangeta@gmail.com>
2021-02-25 15:37:42 -0500

    Update to latest StripeKit

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor-community/stripe-kit.git", from: "10.0.0"),
+        .package(url: "https://github.com/vapor-community/stripe-kit.git", from: "11.0.0"),
     ],
     targets: [
         .target(name: "Stripe", dependencies: [
