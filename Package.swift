93d17d2eae98b3182c8e51237cec04783c5ec75a
Andrew Edwards <andrewangeta@gmail.com>
2022-06-01 01:25:25 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor-community/stripe-kit.git", from: "16.0.0"),
+        .package(url: "https://github.com/vapor-community/stripe-kit.git", from: "16.2.1"),
     ],
     targets: [
         .target(name: "Stripe", dependencies: [
