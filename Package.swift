b37baaeffc890a2c780be60e30631145cdd157d6
Matthew Anisovich <52459150+sourcelocation@users.noreply.github.com>
2023-08-21 12:38:36 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "stripe",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         .library(name: "Stripe", targets: ["Stripe"])
